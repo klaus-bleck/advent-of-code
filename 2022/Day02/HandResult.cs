f9d166cfba13e925fa7fb6337cf64873fc52ca80
Klaus Bleck <klaus.bleck@gmail.com>
2022-12-02 18:50:11 +0100

    Refactoring

---
+++ 2022/Day02/HandResult.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.Day02;
+
+internal abstract class HandResult
+{
+    public abstract Score Score { get; }
+
+    public static HandResult Win { get; } = new WinResult();
+    public static HandResult Lose { get; } = new LoseResult();
+    public static HandResult Draw { get; } = new DrawResult();
+}
+
+internal sealed class WinResult : HandResult
+{
+    public override Score Score => new(6);
+}
+
+internal sealed class DrawResult : HandResult
+{
+    public override Score Score => new(3);
+}
+
+internal sealed class LoseResult : HandResult
+{
+    public override Score Score => new(0);
+}
