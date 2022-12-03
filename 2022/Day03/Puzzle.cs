de18522ea63de837f1abfe3c9bfc389c47dd9161
Klaus Bleck <klaus.bleck@gmail.com>
2022-12-03 19:45:32 +0100

    Solution for day 3

---
+++ 2022/Day03/Puzzle.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode.Day03;
+
+internal class Puzzle : IPuzzle
+{
+    private static IEnumerable<string> GetInput() => InputProvider.Iterate(3);
+
+    private static long GetPriority(char item) =>
+        item switch
+        {
+            >= 'a' and <= 'z' => (int)item - 96,
+            >= 'A' and <= 'Z' => (int)item - 38,
+            _ => throw new NotSupportedException($"Item {item} is not supported."),
+        };
+
+    private static char GetDuplicate(string line)
+    {
+        var compartments = line.Chunk(line.Length / 2).ToArray();
+        return compartments[0].First(x => compartments[1].Contains(x));
+    }
+
+    private static char GetDuplicate(string[] lines) => 
+        lines[0].First(x => lines[1].Contains(x) && lines[2].Contains(x));
+
+    public object SolveFirst() => GetInput().Select(GetDuplicate).Sum(x => GetPriority(x));
+    public object SolveSecond() => GetInput().Chunk(3).Select(GetDuplicate).Sum(x => GetPriority(x));
+}
--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AdventOfCode;
-using AdventOfCode.Day02;
+using AdventOfCode.Day03;
 
 PuzzleRunner.Run<Puzzle>();
 //BenchmarkDotNet.Running.BenchmarkRunner.Run<PuzzleBenchmarkRunner<Puzzle>>();
