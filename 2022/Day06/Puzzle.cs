7c1418e233055617ecf746826922ddae2b90c729
Klaus Bleck <klaus.bleck@gmail.com>
2022-12-07 21:09:16 +0100

    Solution for day 6

---
+++ 2022/Day06/Puzzle.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode.Day06;
+
+internal class Puzzle : IPuzzle
+{
+    private const int StartOfPacketMarkerLength = 4;
+    private const int StartOfMessageMarkerLength = 14;
+    private static IEnumerable<string> GetInput() => InputProvider.Iterate(6);
+
+    private static int GetPositionOfStartOfPacketMarker(int startOfPacketMarkerLength)
+    {
+        var line = GetInput().First();
+        var slidingWindow = new LinkedList<char>();
+        var differenceSet = new HashSet<char>();
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            var character = line[i];
+            slidingWindow.AddLast(character);
+
+            if (slidingWindow.Count >= startOfPacketMarkerLength)
+            {
+                differenceSet.Clear();
+                foreach(var charInWindow in slidingWindow)
+                {
+                    differenceSet.Add(charInWindow);
+                }
+
+                if (differenceSet.Count == startOfPacketMarkerLength)
+                {
+                    return i + 1;
+                }
+                slidingWindow.RemoveFirst();
+            }
+        }
+        return -1;
+    }
+
+    public object SolveFirst() => GetPositionOfStartOfPacketMarker(StartOfPacketMarkerLength);
+    public object SolveSecond() => GetPositionOfStartOfPacketMarker(StartOfMessageMarkerLength);
+}
--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AdventOfCode;
-using AdventOfCode.Day05;
+using AdventOfCode.Day06;
 
 PuzzleRunner.Run<Puzzle>();
 //BenchmarkDotNet.Running.BenchmarkRunner.Run<PuzzleBenchmarkRunner<Puzzle>>();
