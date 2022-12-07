fc54759f0a1fd84130bdd0952d1fe0fd7d4de0f8
Klaus Bleck <klaus.bleck@gmail.com>
2022-12-07 20:38:36 +0100

    Fix InputProvider

--- 2022/Utils/InputProvider.cs
+++ 2022/Utils/InputProvider.cs
@@ -1,10 +1,8 @@
-﻿using System.Diagnostics;
-
-namespace AdventOfCode.Utils;
+﻿namespace AdventOfCode.Utils;
 
 internal static class InputProvider
 {
     private static string GetDayForFileName(uint day) => day.ToString().PadLeft(2, '0');
     private static string GetInputFile(uint day) => Path.Combine($"Day{GetDayForFileName(day)}", "input.txt");
-    public static IEnumerable<string> Iterate(uint day) => File.ReadAllLines(GetInputFile(day));
+    public static IEnumerable<string> Iterate(uint day) => File.ReadLines(GetInputFile(day));
 }
